51681bc879785166eb546bc675c37849c237b7ef
mai129 <mai129@uom.edu.gr>
2012-12-12 16:18:50 +0200

    add methods AddCustomer, ViewAllCustomers, ViewCustomer and EditCustomer without implementation

--- src/CustomerUtils.java
+++ src/CustomerUtils.java
@@ -1,4 +1,21 @@
 
 public class CustomerUtils {
 
+	
+	
+	public void AddCustomer() {
+		
+	}
+	
+	public void ViewAllCustomers() {
+		
+	}
+	
+	public void ViewCustomer() {
+		
+	}
+	
+	public void EditCustomer() {
+		
+	}
 }

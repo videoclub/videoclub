8ac08a30da2c8090e4c44562022ed7812f85a33c
mai1226 <mai1226@uom.gr>
2012-12-13 04:09:28 +0200

    New main package main package includes Main Class + MainUtility Class

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MainUtils.printMsg("mai1226: oxi allo karvouno!");
+
+	}
+
+}

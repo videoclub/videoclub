1fb34d480e3104b05964f655be2cf594cd013a9d
mai1226 <mai1226@uom.gr>
2013-01-08 23:11:21 +0200

    readme.txt

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -4,11 +4,9 @@ import java.util.ArrayList;
 
 public interface Dao {
 	
-	//void openConnection();
 	void persist(Object o);
 	abstract ArrayList<Object> getAllItems();
 	abstract ArrayList<Object> getItemDetails(String title);
 	abstract void updateItem(ArrayList<Object> Object);
-	//void closeConnection();
 	
 }
--- src/dao/impl/DaoImpl.java
+++ src/dao/impl/DaoImpl.java
@@ -1,14 +1,11 @@
 package dao.impl;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import dao.Dao;
 
 public abstract class DaoImpl implements Dao {
 	
-	//private EntityManagerFactory emf;
 	private EntityManager em;
 	
 	public void setEntityManager(EntityManager em){
@@ -19,21 +16,10 @@ public abstract class DaoImpl implements Dao {
 		return em;
 	}
 	
-	
-	/*public void openConnection() {
-		emf = Persistence.createEntityManagerFactory("db/videoclub.odb");
-		em = emf.createEntityManager();
-	}*/
-
 	public void persist(Object o) {
 		em.getTransaction().begin();
 		em.persist(o);
 		em.getTransaction().commit();
 	}
-
-	/*public void closeConnection() {
-		em.close();
-		emf.close();	
-	}*/
 
 }
